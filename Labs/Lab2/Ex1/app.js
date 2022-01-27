4a326e606252fe346e7e0e3c708ca10206aece4b
jenhuan1 <jhuang409@gmail.com>
2022-01-26 21:33:09 -0500

    Lab 2

---
+++ Labs/Lab2/Ex1/app.js
@@ -0,0 +1,15 @@
+//Jennifer Huang
+//1/25/2022
+//n220
+
+//setup the application
+//create canvas
+
+function setup() {
+  createCanvas(400, 400);
+  frameRate(1);
+}
+
+function draw() {
+  console.log(frameCount); 
+}
---
+++ Labs/Lab2/Ex2/app.js
@@ -0,0 +1,21 @@
+//Jennifer Huang
+//1/25/2022
+//n220
+
+//setup the application
+//create canvas
+function setup() {
+    createCanvas(400,300);
+  }
+//draw
+  function draw() {
+    background(50);
+    fill(46, 65, 232);
+    circle(mouseX, mouseY, 30, 30);
+
+    
+    if(mouseX > 200){
+        fill(240, 50, 50);
+        circle(mouseX, mouseY, 30, 30);
+    }
+  }
